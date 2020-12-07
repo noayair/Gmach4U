a5d77d08c3d73fd37fa4325585297e5cb719575d
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2020-12-07 16:26:01 +0200

    first commit supplier register!!!

--- app/src/main/java/Adapters/Supplier.java
+++ app/src/main/java/Adapters/Supplier.java
@@ -1,4 +1,48 @@
 package Adapters;
 
 public class Supplier {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String openingTime;
+
+    public Supplier(String name, String email, String phone, String address, String openingTime){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phone;
+        this.address = address;
+        this.openingTime = openingTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phoneNumber;
+    }
+    public void setPhone(String phone) {
+        this.phoneNumber = phone;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getOpeningTime() {
+        return openingTime;
+    }
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime;
+    }
 }
